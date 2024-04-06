e1c06dd77c5a1eacdd5c1b57ae3a848d5bb1080b
Dean <omegadeangrey@gmail.com>
2024-04-06 14:15:30 -0400

    Up to Persist

---
+++ P0/SongBook/Menu.cs
@@ -0,0 +1,27 @@
+namespace SongBook;
+
+
+class Menu {
+
+public static void ListMenu() {
+    Console.WriteLine("1. Record a New Song");
+    Console.WriteLine("2. View SongBook");
+    Console.WriteLine("Press 9 to exit");
+}
+    public static int UserSelection()
+    {
+        
+        try {
+            return Convert.ToInt32(Console.ReadLine());
+        } catch(Exception e) {
+            Console.WriteLine(e.Message + "Invalid Selection");
+            return -1;
+        }
+        
+        
+    }
+
+}
+
+
+
--- P0/SongBook/Program.cs
+++ P0/SongBook/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       List<Song> songList = new();
+
+    //    try {
+    //     songList = Data.LoadSongs();
+    //    }
+
+    int UserInput = 0;
+
+    while (UserInput !=9) {
+        Menu.ListMenu();
+
+        UserInput = Menu.UserSelection();
+
+        if (UserInput == 1) {
+            songList.Add(Logic.GenerateSong());
+        } else if (UserInput == 2) {
+            Logic.DisplaySongs(songList);
+        } else if (UserInput == 9) {
+            Console.WriteLine("Until Next Time");
+        } else {
+            Console.WriteLine("Pick a Valid Option");
+        }
+
+
+    }
+
     }
 }
